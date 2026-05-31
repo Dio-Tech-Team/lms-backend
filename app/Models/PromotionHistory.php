f686b4785ba30f8d96eb6e3f59b4c947d8364ea5
JhonFarillon <jhonfarillon1206@gmail.com>
2026-05-31 19:55:27 +0800

    Fix promotion history name

--- app/Models/PromotionHistory.php
+++ app/Models/PromotionHistory.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class PromotionHistory extends Model
 {
+    protected $table = 'promotion_history'; // ← add this
+
     protected $fillable = [
         'employee_id',
         'previous_position',
         'new_position',
         'promotion_date',
     ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
