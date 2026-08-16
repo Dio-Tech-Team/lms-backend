edbcb6c0399dd04511e0604a4434c6e11912a24b
JhonFarillon <jhonfarillon1206@gmail.com>
2026-08-16 20:02:38 +0800

    Update EmployeeSeeder and PositionSeeder for presentation demo data

--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -10,11 +10,28 @@ class PositionSeeder extends Seeder
     public function run(): void
     {
         $positions = [
+
+            // Casual And Permanent Positions
             'Administrative Aide I',
+            'Administrative Aide II',
             'Administrative Aide III',
             'Administrative Assistant I',
+            'Administrative Assistant II',
+            'Administrative Assistant III',
             'Administrative Officer I',
-            'HR Officer I',
+            'Administrative Officer II',
+            'Administrative Officer III',
+            'HRMO I',
+            'HRMO II',
+            'HRMO III',
+
+            // Job Order Positions
+            'Encoder',
+
+            //Elected Officials
+            'Sanguniang Bayan',
+            'Municipal Mayor',
+            'Municipal Vice Mayor',
             // ...rest of the plantilla list
         ];
 
